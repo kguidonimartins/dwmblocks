04fbd5becaa14f42c7b811f01110407947a9814b
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2020-11-11 19:11:28 -0300

    feat(bin/statusbar): set the relative path to the scripts components of the dwmblocks
    this makes easy to access the file content using the `gf` command in vim

--- config.h
+++ config.h
@@ -1,20 +1,26 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
+
+// change config here:
+// ~/.local/suckless/dwm/config.def.h
+// ~/.local/suckless/dwm/bindings.h
+
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	/* {"", "system_py",					5,		0}, */
-	{"", "counttmuxsessions",			5,		5},
-	{"", "pacpackages",					0,		8},
-	/* {"", "mailbox",						5,		0}, */
-	/* {"", "music",						5,		10}, */
-	{"", "volume",						5,		10},
-	{"", "internet",					5,		4},
-	/* {"", "kbd_layout",					5,		0}, */
-	{"", "calendar",					5,		0},
-	{"", "clock",						60,		0},
+	{"", "cat /tmp/recordingicon 2>/dev/null",	        0,	    9},
+	{"", "~/.local/bin/statusbar/system_py",			5,		0},
+	{"", "~/.local/bin/statusbar/counttmuxsessions",	5,		5  },
+	{"", "~/.local/bin/statusbar/pacpackages",			0,		8  },
+	{"", "~/.local/bin/statusbar/mailbox",				5,		0  },
+	/* {"", "~/.local/bin/statusbar/music",				5,		10 }, */
+	{"", "~/.local/bin/statusbar/volume",				5,		10 },
+	{"", "~/.local/bin/statusbar/internet",				5,		4  },
+	/* {"", "~/.local/bin/statusbar/kbd_layout",			5,		0  }, */
+	{"", "~/.local/bin/statusbar/calendar",				5,		0  },
+	{"", "~/.local/bin/statusbar/clock",				60,		0  },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char *delim = " ";
+static char *delim = "  ";
 /* static unsigned int delimLen = 1; */
 
 // autocmd BufWritePost ~/.config/suckless/dwmblocks/config.h !cd ~/.config/suckless/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
