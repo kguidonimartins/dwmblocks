116b5e49126ada1f0da15b0935ed7865abc89997
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2020-11-26 12:12:26 -0300

    update path to scripts

--- config.h
+++ config.h
@@ -7,18 +7,18 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "cat /tmp/recordingicon 2>/dev/null",	        0,	    9},
-	/* {"", "~/.local/bin/statusbar/system_py",			5,		0}, */
-	{"", "~/.local/bin/statusbar/spotify",    			5,		0},
-	/* {"", "~/.local/bin/statusbar/docker",			5,		0}, */
-	{"", "~/.local/bin/statusbar/counttmuxsessions",	5,		5  },
-	{"", "~/.local/bin/statusbar/pacpackages",			0,		8  },
-	{"", "~/.local/bin/statusbar/mailbox",				5,		0  },
-	/* {"", "~/.local/bin/statusbar/music",				5,		10 }, */
-	{"", "~/.local/bin/statusbar/volume",				5,		10 },
-	{"", "~/.local/bin/statusbar/internet",				10,		4  },
-	/* {"", "~/.local/bin/statusbar/kbd_layout",			5,		0  }, */
-	{"", "~/.local/bin/statusbar/calendar",				5,		0  },
-	{"", "~/.local/bin/statusbar/clock",				1,		0  },
+	/* {"", "~/.local/bin/tools/statusbar/system_py",			5,		0}, */
+	{"", "~/.local/bin/tools/statusbar/spotify",    			5,		0},
+	/* {"", "~/.local/bin/tools/statusbar/docker",			5,		0}, */
+	{"", "~/.local/bin/tools/statusbar/counttmuxsessions",	5,		5  },
+	{"", "~/.local/bin/tools/statusbar/pacpackages",			0,		8  },
+	{"", "~/.local/bin/tools/statusbar/mailbox",				5,		0  },
+	/* {"", "~/.local/bin/tools/statusbar/music",				5,		10 }, */
+	{"", "~/.local/bin/tools/statusbar/volume",				5,		10 },
+	{"", "~/.local/bin/tools/statusbar/internet",				10,		4  },
+	/* {"", "~/.local/bin/tools/statusbar/kbd_layout",			5,		0  }, */
+	{"", "~/.local/bin/tools/statusbar/calendar",				5,		0  },
+	{"", "~/.local/bin/tools/statusbar/clock",				1,		0  },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
