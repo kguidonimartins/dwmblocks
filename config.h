88ff9a35658e42a0521b40d01de2197ebf7d905e
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2020-12-03 14:32:02 -0300

    put music applet far left

--- config.h
+++ config.h
@@ -7,13 +7,13 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "cat /tmp/recordingicon 2>/dev/null",	        0,	    9},
+	{"", "~/.local/bin/tools/statusbar/music",				5,		10 },
 	/* {"", "~/.local/bin/tools/statusbar/system_py",			5,		0}, */
 	{"", "~/.local/bin/tools/statusbar/spotify",    			5,		0},
 	/* {"", "~/.local/bin/tools/statusbar/docker",			5,		0}, */
 	{"", "~/.local/bin/tools/statusbar/counttmuxsessions",	5,		5  },
 	{"", "~/.local/bin/tools/statusbar/pacpackages",			0,		8  },
 	{"", "~/.local/bin/tools/statusbar/mailbox",				5,		0  },
-	/* {"", "~/.local/bin/tools/statusbar/music",				5,		10 }, */
 	{"", "~/.local/bin/tools/statusbar/volume",				5,		10 },
 	{"", "~/.local/bin/tools/statusbar/internet",				10,		4  },
 	/* {"", "~/.local/bin/tools/statusbar/kbd_layout",			5,		0  }, */
