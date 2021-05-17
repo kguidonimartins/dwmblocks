4b48b67950518dab0fabd2bdba5011efc2106606
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2021-05-17 17:41:19 -0300

    clean up

--- config.h
+++ config.h
@@ -13,8 +13,8 @@
 static const Block blocks[] = {
 /*Icon, Command,                                           Interval,   Signal */
 	{"" , "cat /tmp/recordingicon 2>/dev/null"             , 0    , 9  } ,
-	{"" , "~/.local/bin/tools/statusbar/music"             , 5    , 10 } ,
-	{"" , "~/.local/bin/tools/statusbar/bkpstatus"         , 5    , 10 } ,
+	/* {"" , "~/.local/bin/tools/statusbar/music"             , 5    , 10 } , */
+	/* {"" , "~/.local/bin/tools/statusbar/bkpstatus"         , 5    , 10 } , */
 	{"" , "~/.local/bin/tools/statusbar/ncspot_status"     , 5    , 11 } ,
 	{"" , "~/.local/bin/tools/statusbar/notify_emacs"      , 5    , 5  } ,
 	{"" , "~/.local/bin/tools/statusbar/counttmuxsessions" , 5    , 6  } ,
@@ -25,7 +25,7 @@ static const Block blocks[] = {
 	{"" , "~/.local/bin/tools/statusbar/volume"            , 1    , 12 } ,
 	{"" , "~/.local/bin/tools/statusbar/internet"          , 10   , 4  } ,
 	{"" , "~/.local/bin/tools/statusbar/calendar"          , 3600 , 1  } ,
-	{"" , "~/.local/bin/tools/statusbar/clock"             , 1    , 0  } ,
+	{"" , "~/.local/bin/tools/statusbar/clock"             , 60   , 0  } ,
         {"" , "~/.local/bin/tools/statusbar/notify_googledrive", 5    , 3  } ,
         {"" , "~/.local/bin/tools/statusbar/notify_dropbox"    , 6    , 5  } ,
         {"" , "~/.local/bin/tools/statusbar/notify_redshift"   , 6    , 5  } ,
