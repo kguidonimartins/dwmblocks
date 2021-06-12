7f75874c94716c9bb29f57dd03fc24b9ef5b3d22
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2021-06-12 20:33:10 -0300

    simplify dwmblocks

--- config.h
+++ config.h
@@ -14,10 +14,10 @@ static const Block blocks[] = {
 /*Icon, Command,                                           Interval,   Signal */
 	{"" , "cat /tmp/recordingicon 2>/dev/null"             , 0    , 9  } ,
 	/* {"" , "~/.local/bin/tools/statusbar/music"             , 5    , 10 } , */
-	{"祥 " , "~/.local/bin/tools/statusbar/pomodoro"       , 1    , 21 } ,
+	/* {"祥 " , "~/.local/bin/tools/statusbar/pomodoro"       , 1    , 21 } , */
 	{"" , "~/.local/bin/tools/statusbar/bkpstatus"         , 5    , 10 } ,
 	{"" , "~/.local/bin/tools/statusbar/ncspot_status"     , 5    , 11 } ,
-	{"" , "~/.local/bin/tools/statusbar/notify_emacs"      , 5    , 5  } ,
+	/* {"" , "~/.local/bin/tools/statusbar/notify_emacs"      , 5    , 5  } , */
 	{"" , "~/.local/bin/tools/statusbar/counttmuxsessions" , 5    , 6  } ,
 	{"" , "~/.local/bin/tools/statusbar/countrsessions"    , 5    , 5  } ,
 	{"" , "~/.local/bin/tools/statusbar/cpu_memory"        , 5    , 17 } ,
@@ -26,9 +26,9 @@ static const Block blocks[] = {
 	{"" , "~/.local/bin/tools/statusbar/volume"            , 1    , 12 } ,
 	{"" , "~/.local/bin/tools/statusbar/internet"          , 10   , 4  } ,
 	{"" , "~/.local/bin/tools/statusbar/calendar"          , 3600 , 1  } ,
-	{"" , "~/.local/bin/tools/statusbar/clock"             , 60   , 0  } ,
+        /* {"" , "~/.local/bin/tools/statusbar/notify_dropbox"    , 6    , 5  } , */
+        {"" , "~/.local/bin/tools/statusbar/clock"             , 60   , 0  } ,
         {"" , "~/.local/bin/tools/statusbar/notify_googledrive", 5    , 3  } ,
-        {"" , "~/.local/bin/tools/statusbar/notify_dropbox"    , 6    , 5  } ,
         {"" , "~/.local/bin/tools/statusbar/notify_redshift"   , 6    , 5  } ,
 };
 
